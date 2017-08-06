5f941738c2c94066d0a693cfa5fc186cd93cd459
송찬호 <chanho@hhi.co.kr>
2017-08-07 02:13:20 +0900

    updated available multiple account per customerid

--- src/main/java/com/bigdata/engineer/banking/system/Bank.java
+++ src/main/java/com/bigdata/engineer/banking/system/Bank.java
@@ -1,14 +1,14 @@
 package com.bigdata.engineer.banking.system;
 
 import com.bigdata.engineer.banking.system.config.BankingConstants;
+import com.bigdata.engineer.banking.system.database.BankDB;
 import com.bigdata.engineer.banking.system.transaction.Transactions;
 import com.bigdata.engineer.banking.system.transaction.WithdrawTransaction;
 import com.bigdata.engineer.event.generator.eventunit.customer.Customer;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 import static com.bigdata.engineer.banking.system.Account.getBalance;
 
@@ -16,7 +16,7 @@ public class Bank {
     private static final Logger logger = LogManager.getLogger(Bank.class);
 
     private static Bank instance = new Bank();
-    private Map<String, Account> accountList = new HashMap<>();//customerid, account
+    private Map<String, List<Account>> bankDB = BankDB.getInstance().getAccountList();//customerid, account
 
     private Bank () {
         logger.info(BankingConstants.LOG_APPENDER + "Bank Open!");
@@ -31,29 +31,34 @@ public class Bank {
 
     public Customer createAccount(Customer customer, int initialDeposit) {
         Account account = new Account(initialDeposit);
-        customer.setAccountID(accountRegister(customer.getCustomerID(), account));
+        customer.setAccountID(addBankDB(customer.getCustomerID(), account));
 
         if(logger.isDebugEnabled()){
-            logger.debug(BankingConstants.LOG_APPENDER + "CustomerID '{}' is assigned AccountID: {}, Init Deposit : {}", customer.getCustomerID(), accountList.get(customer.getCustomerID()).getAccountID(), getBalance(account.getAccountID()));
+            logger.debug(BankingConstants.LOG_APPENDER + "CustomerID '{}' is assigned AccountID: {}, Init Deposit : {}", customer.getCustomerID(), account.getAccountID(), getBalance(account.getAccountID()));
         }
 
         return customer;
     }
 
-    private String accountRegister(String custormerID, Account account) {
-        accountList.put(custormerID, account);
+    private String addBankDB(String custormerID, Account account) {
+        if(bankDB.get(custormerID) != null){
+            bankDB.get(custormerID).add(account);
+        } else {
+            bankDB.put(custormerID, Collections.singletonList(account));
+        }
+
         return account.getAccountID();
     }
 
     public void runTransactions(String customerID, String accountID, Transactions transaction) {
         int balance = 0;
         if(transaction instanceof WithdrawTransaction) {
-            balance = transaction.debitAmount(accountList);//출금잔고
+            balance = transaction.debitAmount(accountID);//출금잔고
             if (logger.isDebugEnabled()){
                 logger.debug(BankingConstants.LOG_APPENDER + "CustomerID '{}' is withdrew AccountID: {}, debitAmount : {}", customerID, accountID, balance);
             }
         } else {
-            balance = transaction.creditAmount(accountList);//입금잔고
+            balance = transaction.creditAmount(accountID);//입금잔고
             if (logger.isDebugEnabled()){
                 logger.debug(BankingConstants.LOG_APPENDER + "CustomerID '{}' is Deposited AccountID: {}, creditAmount : {}", customerID, accountID, balance);
             }
